429d3cecf13fbe8dc17b8509ad9a729c95f0fca7
haris49 <hariis.459@gmail.com>
2026-03-10 11:33:58 +0700

    menambahkan proses perulangan pada algoritma insertion sort

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -29,3 +29,10 @@ void input () {
 void insertionSort() { //procedure insertion sort
     int temp;
     int j;
+
+    for (int i = 0; i < n; i++) 
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
